1a3c672455343f2e01b8112206372a5df0d4cbe0
umairyaquoob <umz78@hotmail.co.uk>
2021-09-01 08:41:12 +0100

    countdown-timer.py

---
+++ countdown-timer.py
@@ -0,0 +1,16 @@
+#Countdown timer program
+import time
+
+def coutdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+        
+    print('Timer completed!')
+    
+t = input('Enter the time in seconds: ')
+
+coutdown(int(t))
